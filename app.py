ba61c3c0db663307c1d3ee10e4755ec8a1a72875
sourabhsi001 <singhsourabh38586@gmail.com>
2026-06-30 15:32:04 +0530

    PostgreSQL Connection

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-con
+from flask import Flask
+from config import Config
+from models import db
+app=Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+@app.route("/")
+def home():
+    return"SITE ON WORKING "
+
+with app.app_context():
+    try:
+        db.engine.connect()
+        print("✅ Database Connected Successfully!")
+    except Exception as e:
+        print("❌ Database Connection Failed!")
+        print(e)
+
+if __name__=="__main__":
+    
+    app.run(debug=True)
